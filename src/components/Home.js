75b25668ec3bdabf32741fa3fe286606045fd5c1
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-28 14:07:06 +0430

    add title

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,14 @@
 /** @format */
+import "../style/App.css";
 
 const Home = () => {
-  return <h1>Hello there!</h1>;
+  return (
+    <div className="container">
+      <div className="title">
+        <h1>Animation Buttons</h1>
+        <p>Hover us and enjoy the satisfying neumorphic animation designs!</p>
+      </div>
+    </div>
+  );
 };
 export default Home;
