69b049b78e45f167d6dc4eb685a3699f233732ff
atefehmohammaddoust <atefeh.mohammaddoust@gmail.com>
2022-05-30 16:24:59 +0430

    add click handler as props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,14 @@
 /** @format */
 
 import React from "react";
-const Button = ({ kind, handleClick }) => {
+const Button = ({ kind, width, text, ...props }) => {
+  console.log(props);
   return (
-    <button className={`custom-btn ${kind}`} onClick={handleClick}>
-      <span>Read more</span>
+    <button
+      className={`custom-btn ${kind}`}
+      style={{ width: `${width}` }}
+      {...props}>
+      <span>{text}</span>
     </button>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -15,21 +15,26 @@ const Home = () => {
         <p>Hover us and enjoy the satisfying neumorphic animation designs!</p>
       </div>
       <div className="frame">
-        <Button kind="btn-1" handleClick={handleClick} />
-        <Button kind="btn-2" handleClick={handleClick2} />
-        <Button kind="btn-3" />
-        <Button kind="btn-4" />
-        <Button kind="btn-5" />
-        <Button kind="btn-6" />
-        <Button kind="btn-7" />
-        <Button kind="btn-8" />
-        <Button kind="btn-9" />
-        <Button kind="btn-10" />
-        <Button kind="btn-11" />
-        <Button kind="btn-13" />
-        <Button kind="btn-14" />
-        <Button kind="btn-15" />
-        <Button kind="btn-16" />
+        <Button
+          kind="btn-1"
+          onClick={handleClick}
+          width="100%"
+          text="Read more"
+        />
+        <Button kind="btn-2" handleClick={handleClick2} text="Read more" />
+        <Button kind="btn-3" text="Read more" />
+        <Button kind="btn-4" text="Read more" />
+        <Button kind="btn-5" text="Read more" />
+        <Button kind="btn-6" text="Read more" />
+        <Button kind="btn-7" text="Read more" />
+        <Button kind="btn-8" text="Read more" />
+        <Button kind="btn-9" text="Read more" />
+        <Button kind="btn-10" text="Read more" />
+        <Button kind="btn-11" text="Read more" />
+        <Button kind="btn-13" text="Read more" />
+        <Button kind="btn-14" text="Read more" />
+        <Button kind="btn-15" text="Read more" />
+        <Button kind="btn-16" text="Read more" />
       </div>
     </div>
   );
